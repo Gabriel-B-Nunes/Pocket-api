d9a607928133fdfd22e9b142d1b358dec5183ff0
GabrielBN <gabrielbeckernunes@gmail.com>
2026-08-22 15:27:19 -0400

    refactor: remove remaining unnecessary parameter table

--- init-scripts/0parameter.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE parameter (
-    name VARCHAR(100) NOT NULL PRIMARY KEY,
-    parameterType INT NOT NULL,
-    value VARCHAR(255),
-    description VARCHAR(255) NOT NULL,
-    required BOOL NOT NULL
-);
