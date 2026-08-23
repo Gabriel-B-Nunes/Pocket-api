7178660f79916f26c06960d21d72769661edfe5e
GabrielBN <gabrielbeckernunes@gmail.com>
2026-08-22 21:21:36 -0400

    fix: remove unnecessary userid from usercreatedto

--- var/www/html/dto/UserCreateDTO.php
+++ var/www/html/dto/UserCreateDTO.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
+namespace App\dto;
+
 class UserCreateDTO {
     public function __construct(
-        public readonly int $userId,
         public readonly string $userName,
         public readonly string $userEmail,
         public readonly string $userCellphoneNumber,
