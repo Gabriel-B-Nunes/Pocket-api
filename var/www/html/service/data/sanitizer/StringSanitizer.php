5aa9269cc4fd452bf440eab7171cf37680a8d162
GabrielBN <gabrielbeckernunes@gmail.com>
2026-08-29 20:58:38 -0400

    feat: create StringSanitizer.php and sanitize method

---
+++ var/www/html/service/data/sanitizer/StringSanitizer.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\service\data\sanitizer;
+
+use Normalizer;
+
+class StringSanitizer implements SanitizerInterface {
+
+    public function __construct(
+        private int $normalizationForm = Normalizer::FORM_C,
+        private bool $trim = true,
+        private bool $removeAccents = false,
+        private bool $lowerCase = false,
+        private bool $upperCase = false
+    )
+    {}
+
+    public function sanitize(mixed $string): mixed
+    {
+        if (!is_string($string)) {
+            return null;
+        }
+
+        $tmpString = $string;
+
+        if ($this->removeAccents) {
+            $tmpString = Normalizer::normalize($tmpString, Normalizer::FORM_D);
+            $tmpString = preg_replace("/\p{M}/u", "", $tmpString);
+        }
+
+        $tmpString = Normalizer::normalize($tmpString, $this->normalizationForm);
+
+        if ($this->lowerCase) {
+            $tmpString = mb_strtolower($tmpString);
+        } else if ($this->upperCase) {
+            $tmpString = mb_strtoupper($tmpString);
+        }
+
+        if ($this->trim) {
+            $tmpString = mb_trim($tmpString);
+        }
+
+        return $tmpString;
+    }
+}
