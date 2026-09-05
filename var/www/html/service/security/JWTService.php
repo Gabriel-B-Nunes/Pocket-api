00fd051269e0af0c105bd7da5d6e89d804762858
GabrielBN <gabrielbeckernunes@gmail.com>
2026-09-05 19:30:41 -0400

    feat: create JWTService.php with create and validate methods

---
+++ var/www/html/service/security/JWTService.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\service\security;
+
+use DateTime;
+
+require_once $_SERVER["DOCUMENT_ROOT"] . '/autoload.php';
+
+Class JWTService {
+    public static function create(int $id): string {
+        $header = base64_encode(json_encode([
+            "alg" => "HS256",
+            "typ" => "JWT"
+        ]));
+
+        $payload = base64_encode(json_encode([
+            "sub" => $id,
+            "exp" => new DateTime()->modify("+1 hour")->getTimestamp()
+        ]));
+
+        $secret = getenv("JWT_SECRET");
+
+        $signature = hash_hmac("sha256", "$header.$payload", $secret, true);
+
+        $jwtToken = $header . "." . $payload . "." . $signature;
+
+        return $jwtToken;
+    }
+
+    public static function validate(string $jwtToken): bool {
+        $explodedJwtToken = explode(".", $jwtToken);
+
+        $header = $explodedJwtToken[0] ?? "";
+        $payload = $explodedJwtToken[1] ?? "";
+        
+        $secret = getenv("JWT_SECRET");
+
+        $signature = hash_hmac("sha256", "$header.$payload", $secret, true);
+
+        $expectedToken = $header . "." . $payload . "." . $signature;
+
+        return hash_equals($expectedToken, $jwtToken);
+    }
+}
