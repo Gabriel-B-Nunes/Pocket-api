c44ba9b6c91cde6f06b9d58749254e49960770c4
GabrielBN <gabrielbeckernunes@gmail.com>
2026-09-04 21:20:43 -0400

    feat: create NotFoundException

---
+++ var/www/html/exception/NotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\exception;
+
+require_once $_SERVER["DOCUMENT_ROOT"] . '/autoload.php';
+
+use Exception;
+
+class NotFoundException extends Exception {
+    
+    public function __construct(string $message = "The requested URL was not found on this server", int $code = 404)
+    {
+        return parent::__construct($message, $code);
+    }
+}
