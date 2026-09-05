d70c90833f68231d90c64d513fedcb4943e27602
GabrielBN <gabrielbeckernunes@gmail.com>
2026-09-05 15:09:22 -0400

    feat: create UserLoginDTO.php

---
+++ var/www/html/dto/UserLoginDTO.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\dto;
+
+require_once $_SERVER["DOCUMENT_ROOT"] . '/autoload.php';
+
+use App\service\data\sanitizer\StringSanitizer;
+use App\service\data\validator\StringValidator;
+
+class UserLoginDTO {
+    public function __construct(
+        #[StringSanitizer()]
+        #[StringValidator(validEmail: true)]
+        public readonly string $userEmail,
+
+        #[StringSanitizer()]
+        #[StringValidator(
+            minLength: 12,
+            maxLength: 64,
+            mustContainNumbers: true,
+            mustContainSpecialCharacters: true
+        )]
+        public readonly string $userPassword,
+    )
+    {}
+}
